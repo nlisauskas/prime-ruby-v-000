9fea6f9957629ac00a6790ebc40765852b064863
Nick Lisauskas <nlisausk@alumni.nd.edu>
2017-10-11 02:13:45 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,15 @@
 # Add  code here!
 def prime?(integer)
   i = 2
+  if integer.to_i == 1 || integer.to_i == -1
+    return false
+  else
   while i < integer.to_i.abs
     if integer.to_i % i == 0
       return false
     end
     i += 1
   end
+end
   true
 end
